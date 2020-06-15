7536b5dc95559c14526f7b8cbd44220f6963563d
frank <paintersmechanic@gmail.com>
2020-06-15 12:45:15 -0700

    second commit

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import MovieViewSet, RatingViewSet
+from api.views import MovieViewSet, RatingViewSet, UserViewSet
 
 router = routers.DefaultRouter()
+router.register('users', UserViewSet)
 router.register('movies', MovieViewSet)
 router.register('rating', RatingViewSet)
 
--- api/views.py
+++ api/views.py
@@ -1,17 +1,25 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Movie, Rating
-from .serializers import MovieSerializer, RatingSerializer
+from .serializers import MovieSerializer, RatingSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
   queryset = Movie.objects.all()
   serializer_class = MovieSerializer
-  authentication_classes = (TokenAuthentication, )
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (IsAuthenticated)
 
   # useless action.....
   @action(detail=True, methods=['POST'])
@@ -38,9 +46,19 @@ class MovieViewSet(viewsets.ModelViewSet):
       return Response(message, status=status.HTTP_200_OK)
     else:
       message = {'message': 'you ndeed to provide stars'}
-      return Response(message, status= status.HTTP_400_BAD_REQUEST)
+      return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    
 
 class RatingViewSet(viewsets.ModelViewSet):
   queryset = Rating.objects.all()
   serializer_class = RatingSerializer
   authentication_classes = (TokenAuthentication, )
+  permission_classes = (IsAuthenticated)
+
+  def update(self, request, *args, **kwargs):
+    message = {'message': 'Cannot update rating like that'}
+    return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+  def create(self, request, *args, **kwargs):
+    message = {'message': 'Cannot create rating like that'}
+    return Response(message, status=status.HTTP_400_BAD_REQUEST)
